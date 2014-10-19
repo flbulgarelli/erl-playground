6c8f634e0e395f9c7363e384e221afa37f004605
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2014-10-19 17:35:12 -0300

    Alternative modelling

---
+++ 2048-alt/board.erl
@@ -0,0 +1,81 @@
+-module(board).
+-compile(export_all).
+
+start() ->
+  spawn(?MODULE, init, [4]).
+
+init(Size) ->
+  Cells = create_cells(Size),
+  loop({Cells, Cells, create_compressors(Size), Size}).
+
+create_cells(Size) ->
+  lists:duplicate(Size, lists:duplicate(Size, nil)).
+
+create_compressors(Size) ->
+  [ compressor:start() || _ <- lists:seq(1, Size) ].
+
+loop(S = {Rows, Columns, Compressors, Size}) ->
+  receive 
+    {move, right} -> 
+      do_compress(Size, row, Rows, Compressors),
+      loop(S);
+    {move, down} ->
+      do_compress(Size, column, Columns, Compressors),
+      loop(S);
+    {set_slice, Type, Slice, Index} ->
+      loop(update_matrix(Type, Slice, Index, S));
+    {set_value, X, Y, Value} ->
+      S1 = update_matrix(row, new_slice(X, Y, Value, Rows), Y, S),
+      S2 = update_matrix(column, new_slice(Y, X, Value, Columns), X, S1),
+      loop(S2);
+    {Pid, Ref, render} ->
+      Pid ! {Ref, Rows},
+      loop(S)
+  end.
+
+update_matrix(Type, Slice, Index, {Rows, Columns, Compressors, Size}) ->
+  case Type of 
+    row ->
+      {setnth(Index, Slice, Rows), set_orthogonal(Index, Slice, Columns), Compressors, Size};
+    column ->
+      {set_orthogonal(Index, Slice, Rows), setnth(Index, Slice, Columns), Compressors, Size}
+  end.
+
+set_orthogonal(Index, Slice, OrthogonalSlices) ->
+   [ setnth(Index, Cell, OrthogonalSlice) || 
+      {Cell, OrthogonalSlice} <- lists:zip(Slice, OrthogonalSlices) ].
+
+new_slice(C1, C2, Value, Slice) ->
+  setnth(C2, Value, lists:nth(C1, Slice)).
+
+do_compress(Size, Type, Slices, Compressors) -> 
+  lists:foreach(fun({Slice, Compressor, Index}) ->
+    compressor:compress_slice(Compressor, Type, Slice, Index)
+  end, lists:zip3(Slices, Compressors, lists:seq(1, Size))).
+
+setnth(Index, Value, Xs) -> setnth1(Index, Value, Xs, 1).
+
+setnth1(Index, Value, [_|Xs], N) when N =:= Index -> [Value|Xs];
+setnth1(Index, Value, [X|Xs], N) -> [X|setnth1(Index, Value, Xs, N + 1)].
+
+render(Pid) ->
+  Ref = make_ref(),
+  Pid ! { self(), Ref, render},
+  receive
+    {Ref, Cells} -> Cells
+  after 5000 ->
+    timeout
+  end.
+
+move(Pid, Direction) ->
+   Pid ! {move, Direction}.
+
+set_value(Pid, X, Y, Value) ->
+  Pid ! { set_value, X, Y, value_for(Value) }.
+
+set_slice(Pid, Type, Slice, Index) ->
+  Pid ! {set_slice, Type, Slice, Index}.
+
+value_for(nil) -> nil;
+value_for(N) -> {val, N}.
+
